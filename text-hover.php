8749da21c03fd616a64c77b448c624ec56a3b3cb
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-21 19:25:37 +0000

    * Update to v3.0 * Re-implementation by extending C2C_Plugin_015, which among other things adds support for:     * Reset of options to default values     * Better sanitization of input values     * Offload of core/basic functionality to generic plugin framework     * Additional hooks for various stages/places of plugin operation     * Easier localization support * Full localization support * Disable auto-wrapping of text in the textarea input field for hovers * Allow filtering of text hover terms and replacement via 'c2c_text_hover' filter * Allow filtering of hooks that get text hover processing via 'c2c_text_hover_filters' filter * Allow filtering/overriding of case_sensitive option via 'c2c_text_hover_case_sensitive' filter * Filter 'widget_text' for text hover * Rename class from 'TextHover' to 'c2c_TextHover' * Assign object instance to global variable, $c2c_text_hover, to allow for external manipulation * Remove docs from top of plugin file (all that and more are in readme.txt) * Update readme.txt * Minor code reformatting (spacing) * Add Filters and Upgrade Notice sections to readme.txt * Note compatibility with WP 3.0+ * Drop support for versions of WordPress older than 2.8 * Add .pot file * Update screenshot * Add PHPDoc documentation * Add package info to top of file * Update copyright date * Remove trailing whitespace
    git-svn-id: https://plugins.svn.wordpress.org/text-hover/trunk@292151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- text-hover.php
+++ text-hover.php
@@ -1,33 +1,32 @@
 <?php
+/**
+ * @package Text_Hover
+ * @author Scott Reilly
+ * @version 3.0
+ */
 /*
 Plugin Name: Text Hover
-Version: 2.2
-Plugin URI: http://coffee2code.com/wp-plugins/text-hover
+Version: 3.0
+Plugin URI: http://coffee2code.com/wp-plugins/text-hover/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
+Text Domain: text-hover
 Description: Add hover text to regular text in posts. Handy for providing explanations of names, terms, phrases, and acronyms mentioned in your blog.
 
-Compatible with WordPress 2.6+, 2.7+, 2.8+.
+Compatible with WordPress 2.8+, 2.9+, 3.0+.
 
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-INSTALLATION:
-
-1. Download the file http://coffee2code.com/wp-plugins/text-hover.zip and unzip it into your /wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Go to the Settings -> Text Hover admin settings page (which you can also get to via the Settings link next to the plugin on
-the Manage Plugins page) and customize the settings (namely to define the terms/acronyms and their explanations).
-4. Use the terms/acronyms in posts and/or pages (terms/acronyms appearing in existing posts will also be affected by this plugin)
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/text-hover/
 
 */
 
 /*
-Copyright (c) 2007-2009 by Scott Reilly (aka coffee2code)
+Copyright (c) 2007-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -38,266 +37,111 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-if ( !class_exists('TextHover') ) :
 
-class TextHover {
-	var $admin_options_name = 'c2c_text_hover';
-	var $nonce_field = 'update-text_hover';
-	var $show_admin = true;	// Change this to false if you don't want the plugin's admin page shown.
-	var $config = array();
-	var $options = array(); // Don't use this directly
-	var $plugin_basename = '';
-	var $plugin_name = '';
-	var $menu_name = '';
+if ( !class_exists( 'c2c_TextHover' ) ) :
 
-	function TextHover() {
-		$this->plugin_basename = plugin_basename(__FILE__);
-		$this->plugin_name = __('Text Hover');
-		$this->menu_name = __('Text Hover', 'text-hover');
+require_once( 'c2c-plugin.php' );
+
+class c2c_TextHover extends C2C_Plugin_015 {
+
+	/**
+	 * Handles installation tasks, such as ensuring plugin options are instantiated and saved to options table.
+	 *
+	 * @return void
+	 */
+	function c2c_TextHover() {
+		$this->C2C_Plugin_015( '3.0', 'text-hover', 'c2c', __FILE__, array() );
+	}
+
+	/**
+	 * Override the plugin framework's register_filters() to actually actions against filters.
+	 *
+	 * @return void
+	 */
+	function register_filters() {
+		$filters = apply_filters( 'c2c_text_hover_filters', array( 'the_content', 'get_the_excerpt', 'widget_text' ) );
+		foreach ( (array) $filters as $filter )
+			add_filter( $filter, array( &$this, 'text_hover' ), 3 );
+	}
+
+	/**
+	 * Initializes the plugin's configuration and localizable text variables.
+	 *
+	 * @return void
+	 */
+	function load_config() {
+		$this->name = __( 'Text Hover', $this->textdomain );
+		$this->menu_name = __( 'Text Hover', $this->textdomain );
+
 		$this->config = array(
-			// input can be 'checkbox', 'text', 'textarea', 'hidden', or 'none'
-			'text_to_hover' => array('input' => 'textarea', 'datatype' => 'hash', 'default' => array(
+			'text_to_hover' => array( 'input' => 'textarea', 'datatype' => 'hash', 'default' => array(
 					"WP" => "WordPress"
-				), 'label' => '',
-				'help' => '',
-				'input_attributes' => 'style="width: 98%; font-family: \"Courier New\", Courier, mono;" rows="15" cols="40"'
+				), 'allow_html' => true, 'no_wrap' => true, 'input_attributes' => 'rows="15" cols="40"',
+				'label' => '', 'help' => ''
 			),
-			'case_sensitive' => array('input' => 'checkbox', 'default' => true,
-				'label' => 'Should the matching of terms/acronyms be case sensitive?',
-				'help' => 'i.e. if you define a term of \'WP\', should \'wp\' also be treated the same way? This setting applies to all terms. If you want to selectively have case insensitive terms, then leave this option checked and create separate entries for each variation.')
+			'case_sensitive' => array( 'input' => 'checkbox', 'default' => true,
+				'label' => __( 'Should the matching of terms/acronyms be case sensitive?', $this->textdomain ),
+				'help' => __( 'i.e. if you define a term of \'WP\', should \'wp\' also be treated the same way? This setting applies to all terms. If you want to selectively have case insensitive terms, then leave this option checked and create separate entries for each variation.', $this->textdomain )
+			)
 		);
-
-		add_action('admin_menu', array(&$this, 'admin_menu'));		
-
-		add_filter('the_content', array(&$this, 'text_hover'), 2);
-		add_filter('get_the_excerpt', array(&$this, 'text_hover'), 2);
 	}
 
-	function install() {
-		$this->options = $this->get_options();
-		update_option($this->admin_options_name, $this->options);
+	/**
+	 * Outputs the text above the setting form
+	 *
+	 * @return void (Text will be echoed.)
+	 */
+	function options_page_description() {
+		parent::options_page_description( __( 'Text Hover Settings', $this->textdomain ) );
+
+		echo '<p>' . __( 'Text Hover is a plugin that allows you to add hover text for text in posts. Very handy to create hover explanations of people mentioned in your blog, and/or definitions of unique acronyms and terms you use.', $this->textdomain ) . '</p>';
+		echo '<div class="c2c-hr">&nbsp;</div>';
+		echo '<h3>' . __( 'Acronyms and hover text', $this->textdomain ) . '</h3>';
+		echo '<p>' . __( 'Define terms/acronyms and hovertext explanations here.  The format should be like this:', $this->textdomain ) . '</p>';
+		echo "<blockquote><code>WP => WordPress</code></blockquote>";
+		echo '<p>' . __( 'Where <code>WP</code> is the term, acronym, or phrase you intend to use in your posts, and the <code>WordPress</code> would be what you want to appear in a hover tooltip when a visitor hovers their mouse over the term.', $this->textdomain ) . '</p>';
+		echo '<p>' . __( 'Other considerations:', $this->textdomain ) . '</p>';
+		echo '<ul class="c2c-plugin-list"><li>';
+		echo __( 'Terms and acronyms are assumed to be whole words within your posts (i.e. they are immediately prepended by some sort of space character (space, tab, etc) and are immediately appended by a space character or punctuation (which can include any of: ?!.,-+)]})', $this->textdomain );
+		echo '</li><li>';
+		echo __( 'Only use quotes it they are actual part of the original or hovertext strings.', $this->textdomain );
+		echo '</li><li><strong><em>';
+		echo __( 'Define only one hovertext per line.', $this->textdomain );
+		echo '</em></strong></li><li><strong><em>';
+		echo __( 'Hovertexts must not span multiple lines.', $this->textdomain );
+		echo '</em></strong></li><li><strong><em>';
+		echo __( 'Don\'t use HTML in the hovertext.', $this->textdomain );
+		echo '</em></strong></li></ul>';
 	}
 
-	function admin_menu() {
-		if ( $this->show_admin ) {
-			global $wp_version;
-			if ( current_user_can('manage_options') ) {
-				if ( version_compare( $wp_version, '2.6.999', '>' ) )
-					add_filter( 'plugin_action_links_' . $this->plugin_basename, array(&$this, 'plugin_action_links') );
-				add_options_page($this->menu_name, $this->menu_name, 9, $this->plugin_basename, array(&$this, 'options_page'));
-			}
-		}
-	}
-
-	function plugin_action_links( $action_links ) {
-		$settings_link = '<a href="options.php?page='.$this->plugin_basename.'">' . __('Settings') . '</a>';
-		array_unshift( $action_links, $settings_link );
-
-		return $action_links;
-	}
-
-	function get_options() {
-		if ( !empty($this->options) ) return $this->options;
-		// Derive options from the config
-		$options = array();
-		foreach ( array_keys($this->config) as $opt ) {
-			$options[$opt] = $this->config[$opt]['default'];
-		}
-        $existing_options = get_option($this->admin_options_name);
-        if ( !empty($existing_options) ) {
-            foreach ( $existing_options as $key => $value )
-                $options[$key] = $value;
-        }            
-		$this->options = $options;
-        return $options;
-	}
-
-	function options_page() {
-		$options = $this->get_options();
-		// See if user has submitted form
-		if ( isset($_POST['submitted']) ) {
-			check_admin_referer($this->nonce_field);
-
-			foreach ( array_keys($options) AS $opt ) {
-				$options[$opt] = $_POST[$opt];
-				if ( ($this->config[$opt]['input'] == 'checkbox') && !$options[$opt] )
-					$options[$opt] = 0;
-				if ( $this->config[$opt]['datatype'] == 'array' )
-					$options[$opt] = explode(',', str_replace(array(', ', ' ', ','), ',', $options[$opt]));
-				elseif ( $this->config[$opt]['datatype'] == 'hash' ) {
-					if ( !empty($options[$opt]) ) {
-						$new_values = array();
-						foreach ( explode("\n", $options[$opt]) AS $line ) {
-							list($shortcut, $text) = array_map('trim', explode("=>", $line, 2));
-							if ( !empty($shortcut) ) $new_values[str_replace('\\', '', $shortcut)] = str_replace('\\', '', $text);
-						}
-						$options[$opt] = $new_values;
-					}
-				}
-			}
-			// Remember to put all the other options into the array or they'll get lost!
-			update_option($this->admin_options_name, $options);
-
-			echo "<div id='message' class='updated fade'><p><strong>" . __('Settings saved.', 'text-hover') . '</strong></p></div>';
-		}
-
-		$action_url = $_SERVER[PHP_SELF] . '?page=' . $this->plugin_basename;
-		$logo = plugins_url() . '/' . basename($_GET['page'], '.php') . '/c2c_minilogo.png';
-
-		echo <<<END
-		<div class='wrap'>
-			<div class='icon32' style='width:44px;'><img src='$logo' alt='A plugin by coffee2code' /><br /></div>
-			<h2>{$this->plugin_name} Settings</h2>
-			<p>Text Hover is a plugin that allows you to add hover text for text in posts.
-			   Very handy to create hover explanations of people mentioned in your blog, and/or
-			   definitions of unique acronyms and terms you use. </p>
-
-	<form name="text_hover" action="$action_url" method="post">	
-			<input type="hidden" name="submitted" value="1" />
-			<p>Define terms/acronyms and hovertext explanations here.  The format should be like this:</p>
-
-			<blockquote><code>WP => WordPress</code></blockquote>
-
-			<p>Where <code>WP</code> is the term, acronym, or phrase you intend to use in your posts, and the 
-			<code>WordPress</code> would be what you want to appear in a hover tooltip when a visitor hovers their
-			mouse over the term.</p>
-
-			<p>Other considerations:</p>
-
-			<ul>
-			<li style="list-style:disc outside; margin-left:1em;">Terms and acronyms are assumed to be whole words within your posts (i.e. they are immediately prepended 
-				by some sort of space character (space, tab, etc) and are immediately appended by a space character or 
-				punctuation (which can include any of: ?!.,-+)]})</li>
-			<li style="list-style:disc outside; margin-left:1em;"><strong><em>Define only one hovertext per line.</em></strong></li>
-			<li style="list-style:disc outside; margin-left:1em;"><strong><em>Hovertexts must not span multiple lines (automatic linewrapping is okay).</em></strong></li>
-			<li style="list-style:disc outside; margin-left:1em;"><strong><em>Don't use HTML in the hovertext.</em></strong></li>
-			</ul>
-
-END;
-				wp_nonce_field($this->nonce_field);
-		echo '<table width="100%" cellspacing="2" cellpadding="5" class="optiontable editform form-table">';
-				foreach ( array_keys($options) as $opt ) {
-					$input = $this->config[$opt]['input'];
-					if ( $input == 'none' ) continue;
-					$label = $this->config[$opt]['label'];
-					$value = $options[$opt];
-					if ( $input == 'checkbox' ) {
-						$checked = ($value == 1) ? 'checked=checked ' : '';
-						$value = 1;
-					} else {
-						$checked = '';
-					};
-					if ( $this->config[$opt]['datatype'] == 'array' ) {
-						if ( !is_array($value) )
-							$value = '';
-						else {
-							if ( $input == 'textarea' || $input == 'inline_textarea' )
-								$value = implode("\n", $value);
-							else
-								$value = implode(', ', $value);
-						}
-					} elseif ( $this->config[$opt]['datatype'] == 'hash' ) {
-						if ( !is_array($value) )
-							$value = '';
-						else {
-							$new_value = '';
-							foreach ( $value AS $shortcut => $replacement ) {
-								$new_value .= "$shortcut => $replacement\n";
-							}
-							$value = $new_value;
-						}
-					}
-					echo "<tr valign='top'>";
-					if ( $input == 'textarea' ) {
-						echo "<td colspan='2'>";
-						if ( $label ) echo "<strong>$label</strong><br />";
-						echo "<textarea name='$opt' id='$opt' {$this->config[$opt]['input_attributes']}>" . $value . '</textarea>';
-					} else {
-						echo "<th scope='row'>$label</th><td>";
-						if ( $input == 'inline_textarea' )
-							echo "<textarea name='$opt' id='$opt' {$this->config[$opt]['input_attributes']}>" . $value . '</textarea>';
-						elseif ( $input == 'select' ) {
-							echo "<select name='$opt' id='$opt'>";
-							foreach ( $this->config[$opt]['options'] as $sopt ) {
-								$selected = $value == $sopt ? " selected='selected'" : '';
-								echo "<option value='$sopt'$selected>$sopt</option>";
-							}
-							echo "</select>";
-						} else
-							echo "<input name='$opt' type='$input' id='$opt' value='$value' $checked {$this->config[$opt]['input_attributes']} />";
-					}
-					if ( $this->config[$opt]['help'] ) {
-						echo "<br /><span style='color:#777; font-size:x-small;'>";
-						echo $this->config[$opt]['help'];
-						echo "</span>";
-					}
-					echo "</td></tr>";
-				}
-		$txt = __('Save Changes');
-		echo <<<END
-			</tbody></table>
-			<input type="hidden" name="submitted" value="1" />
-			<div class="submit"><input type="submit" name="Submit" class="button-primary" value="{$txt}" /></div>
-		</form>
-			</div>
-END;
-
-		echo <<<END
-		<style type="text/css">
-			#c2c {
-				text-align:center;
-				color:#888;
-				background-color:#ffffef;
-				padding:5px 0 0;
-				margin-top:12px;
-				border-style:solid;
-				border-color:#dadada;
-				border-width:1px 0;
-			}
-			#c2c div {
-				margin:0 auto;
-				padding:5px 40px 0 0;
-				width:45%;
-				min-height:40px;
-				background:url('$logo') no-repeat top right;
-			}
-			#c2c span {
-				display:block;
-				font-size:x-small;
-			}
-		</style>
-		<div id='c2c' class='wrap'>
-			<div>
-			This plugin brought to you by <a href="http://coffee2code.com" title="coffee2code.com">Scott Reilly, aka coffee2code</a>.
-			<span><a href="http://coffee2code.com/donate" title="Please consider a donation">Did you find this plugin useful?</a></span>
-			</div>
-		</div>
-END;
-	}
-
+	/**
+	 * Perform text hover replacements
+	 *
+	 * @param string $text Text to be processed for text hovers
+	 * @return string Text with hovertexts already processed
+	 */
 	function text_hover( $text ) {
-		$oldchars = array("(", ")", "[", "]", "?", ".", ",", "|", "\$", "*", "+", "^", "{", "}");
-		$newchars = array("\(", "\)", "\[", "\]", "\?", "\.", "\,", "\|", "\\\$", "\*", "\+", "\^", "\{", "\}");
+		$oldchars = array( "(", ")", "[", "]", "?", ".", ",", "|", "\$", "*", "+", "^", "{", "}" );
+		$newchars = array( "\(", "\)", "\[", "\]", "\?", "\.", "\,", "\|", "\\\$", "\*", "\+", "\^", "\{", "\}" );
 		$options = $this->get_options();
 		$text = ' ' . $text . ' ';
-		$text_to_hover = apply_filters($this->admin_options_name.'_option_text_to_hover', $options['text_to_hover']);
+		$text_to_hover = apply_filters( $this->admin_options_name.'_option_text_to_hover', $options['text_to_hover'] ); //legacy (pre-3.0)
+		$text_to_hover = apply_filters( 'c2c_text_hover', $text_to_hover );
+		$case_sensitive = apply_filters( 'c2c_text_hover_case_sensitive', $options['case_sensitive'] );
+		$preg_flags = $case_sensitive ? 's' : 'si';
 		foreach ( $text_to_hover as $old_text => $hover_text ) {
-			$old_text = stripslashes(str_replace($oldchars, $newchars, $old_text));
+			$old_text = stripslashes( str_replace( $oldchars, $newchars, $old_text ) );
 			// WILL match string within string, but WON'T match within tags
-			$preg_flags = ($options['case_sensitive']) ? 's' : 'si';
-			$new_text = "$1<acronym title='" . attribute_escape($hover_text) . "'>$2</acronym>$3";
-			$text = preg_replace("|(?!<.*?)([\s\'\"\.\x98\x99\x9c\x9d\xCB\x9C\xE2\x84\xA2\xC5\x93\xEF\xBF\xBD\(\[\{])($old_text)([\s\'\"\x98\x99\x9c\x9d\xCB\x9C\xE2\x84\xA2\xC5\x93\xEF\xBF\xBD\?\!\.\,\-\+\]\)\}])(?![^<>]*?>)|$preg_flags", $new_text, $text);
+			$new_text = "$1<acronym title='" . esc_attr( $hover_text ) . "'>$2</acronym>$3";
+			$text = preg_replace( "|(?!<.*?)([\s\'\"\.\x98\x99\x9c\x9d\xCB\x9C\xE2\x84\xA2\xC5\x93\xEF\xBF\xBD\(\[\{])($old_text)([\s\'\"\x98\x99\x9c\x9d\xCB\x9C\xE2\x84\xA2\xC5\x93\xEF\xBF\xBD\?\!\.\,\-\+\]\)\}])(?![^<>]*?>)|$preg_flags", $new_text, $text );
 		}
-		return trim($text);
-	} //end text_hover()
+		return trim( $text );
+	}
 
-} // end TextHover
+} // end c2c_TextHover
+
+$GLOBALS['c2c_text_hover'] = new c2c_TextHover();
 
 endif; // end if !class_exists()
 
-if ( class_exists('TextHover') ) :
-	$text_hover = new TextHover();
-	if ( isset($text_hover) )
-		register_activation_hook( __FILE__, array(&$text_hover, 'install') );
-endif;
 ?>
